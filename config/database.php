152142fca09af0fd4f79ab7181286fb08e0dd013
Elliot Bouvier <elliotbouvier@protonmail.com>
2019-03-15 10:27:30 +0100

    update docker compose

--- config/database.php
+++ config/database.php
@@ -11,4 +11,55 @@ $DB_NAME = 'camagru';
 $DB_USER = 'camagru';
 $DB_PASSWORD = 'camagru';
 
+$DB_USERS = "CREATE TABLE IF NOT EXISTS `users` (
+  `id`             int(11)      NOT NULL AUTO_INCREMENT,
+  `username`       varchar(45)  NOT NULL,
+  `password`       varchar(255) NOT NULL,
+  `email`          varchar(45)  NOT NULL,
+  `verified_at`    datetime     DEFAULT NULL,
+  `email_hash`     varchar(80)  DEFAULT NULL,
+  `created_at`     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `password_hash`  varchar(80)  DEFAULT NULL,
+  `receive_emails` tinyint(1)   NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_uindex` (`email`),
+  UNIQUE KEY `users_username_uindex` (`username`)) 
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8";
 
+$DB_IMAGES = "CREATE TABLE IF NOT EXISTS `images`
+(
+  `id`         int(11)      NOT NULL AUTO_INCREMENT,
+  `path`       varchar(255) NOT NULL,
+  `created_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id`    int(11)      DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `images_id_uindex` (`id`),
+  KEY `images_users_id_fk` (`user_id`),
+  CONSTRAINT `images_users_id_fk` FOREIGN KEY(`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE) 
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8";
+
+$DB_IMAGES_HAS_LIKES = "CREATE TABLE IF NOT EXISTS `images_has_likes` (
+  `user_id`  int(11) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  KEY `images_has_likes_images_id_fk` (`image_id`),
+  KEY `images_has_likes_users_id_fk` (`user_id`),
+  CONSTRAINT `images_has_likes_images_id_fk` FOREIGN KEY(`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `images_has_likes_users_id_fk` FOREIGN KEY(`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE) 
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8";
+
+$DB_COMMENTS = "CREATE TABLE IF NOT EXISTS `comments` (
+  `id`         int(11)    NOT NULL AUTO_INCREMENT,
+  `comment`    longtext   NOT NULL,
+  `created_at` timestamp  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id`    int(11)    NOT NULL,
+  `image_id`   int(11)    NOT NULL,
+  PRIMARY KEY(`id`),
+  KEY `comments_images_id_fk` (`image_id`),
+  KEY `comments_users_id_fk` (`user_id`),
+  CONSTRAINT `comments_images_id_fk` FOREIGN KEY(`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comments_users_id_fk` FOREIGN KEY(`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE) 
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8";
--- config/setup.php
+++ config/setup.php
@@ -7,12 +7,19 @@
  */
 
 include 'database.php';
-echo 'LOL';
-//try {
-//    $pdo = new PDO('mysql:host=' . $DB_DSN . ';dbname=' . $DB_NAME . ';charset=utf8', $DB_USER, $DB_PASSWORD);
-//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//} catch (Exception $e) {
-//    echo $e->getMessage();
-//}
-
-exit(0);
+try {
+    $pdo = new PDO('mysql:host=' . $DB_DSN . ';dbname=' . $DB_NAME . ';charset=utf8', $DB_USER, $DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "\033[31mCreating tables... 🔥\033[0m\n";
+    echo "\033[31mCreating tables users... 🔥\033[0m\n";
+    $pdo->query($DB_USERS);
+    echo "\033[31mCreating tables images... 🔥\033[0m\n";
+    $pdo->query($DB_IMAGES);
+    echo "\033[31mCreating tables images_has_likes... 🔥\033[0m\n";
+    $pdo->query($DB_IMAGES_HAS_LIKES);
+    echo "\033[31mCreating tables comments... 🔥\033[0m\n";
+    $pdo->query($DB_COMMENTS);
+    echo "\033[32mDone! 🙆‍♂️\033[0m\n";
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
